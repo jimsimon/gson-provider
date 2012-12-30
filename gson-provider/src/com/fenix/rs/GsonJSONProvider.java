2c47039e9cbedc5afc8539ae90e8c2548b3ef7bc
Jim <Jim@Jim-Laptop>
2012-12-29 21:20:01 -0500

    import cleanup

--- gson-provider/src/com/fenix/rs/GsonJSONProvider.java
+++ gson-provider/src/com/fenix/rs/GsonJSONProvider.java
@@ -11,7 +11,6 @@ import java.lang.reflect.Type;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
